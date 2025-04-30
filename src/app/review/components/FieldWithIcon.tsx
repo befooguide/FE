588619669a4bacf395c2e0a8a191a8994523db8b
choigyulee <cgl1409@gmail.com>
2025-04-30 20:47:03 +0900

    feature: 다녀온 식당 평가 페이지 기능 완료

--- src/app/review/components/FieldWithIcon.tsx
+++ src/app/review/components/FieldWithIcon.tsx
@@ -1,28 +1,42 @@
 "use client";
 
-import React from "react";
-import styled from "styled-components";
+import React, { useState } from "react";
+import styled, { useTheme } from "styled-components";
 
 interface FieldWithIconProps {
   type: string;
-  color?: string;
+  onLikeChange?: (liked: boolean) => void;
 }
 
-const FieldWithIcon: React.FC<FieldWithIconProps> = ({ type, color }) => {
+const FieldWithIcon: React.FC<FieldWithIconProps> = ({ type, onLikeChange }) => {
+  const [liked, setLiked] = useState(false);
+  const theme = useTheme();
+
+  const handleLikeToggle = () => {
+    const newLiked = !liked;
+    setLiked(newLiked);
+    if (onLikeChange) onLikeChange(newLiked);
+  };
+
   return (
     <Container>
       {type === "receipt" && (
-      <img src="/icons/clipboardAdd.svg" alt="영수증 스캔하기" width="24" height="24" /> )}
-            {type === "like" && (
-      <img src="/icons/like.svg" alt="식당 추천하기" width="24" height="24" /> )}
+        <img src="/icons/clipboardAdd.svg" alt="영수증 스캔하기" width="24" height="24" />
+      )}
+
+      {type === "like" && (
+        <LikeIcon
+          $color={liked ? theme.colors.primary : "#ccc"}
+          onClick={handleLikeToggle}
+        />
+      )}
     </Container>
   );
 };
 
 export default FieldWithIcon;
 
-
-const Container = styled.div<{backgroundcolor?: string, width?: string}>`
+const Container = styled.div`
   display: flex;
   width: 21.1875rem;
   height: 5rem;
@@ -33,4 +47,16 @@ const Container = styled.div<{backgroundcolor?: string, width?: string}>`
 
   border-radius: 0.625rem;
   border: 1px solid #DEDEDE;
+`;
+
+const LikeIcon = styled.div<{ $color: string }>`
+  width: 24px;
+  height: 24px;
+  cursor: pointer;
+  background-color: ${(props) => props.$color};
+  mask: url("/icons/like.svg") no-repeat center;
+  -webkit-mask: url("/icons/like.svg") no-repeat center;
+  mask-size: contain;
+  -webkit-mask-size: contain;
+  transition: background-color 0.2s ease;
 `;
